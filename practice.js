b24d6ec9df06f76baf0af1a7146485325c953a6e
Stéfano Mantovani <stefano4d@gmail.com>
2022-01-29 14:49:54 -0300

    Added additional tests

--- practice.js
+++ practice.js
@@ -1,14 +1,12 @@
-function sum(a, b) {
-    return a + b;
+
+
+  function capitalize(inputWord){
+    
+    return (inputWord.charAt(0).toUpperCase())+inputWord.slice(1);
   }
 
- /* function capitalize(inputWord){
-    let test = 'Word';
-    return test.valueOf;
-     //return (inputWord.charAt(0));
-  }*/
 
-function reversion(toReverseWord){
+function reverseString(toReverseWord){
   let reversed = "";
   for(let i = (toReverseWord.length)-1 ; i >= 0; i--){
      reversed += toReverseWord.charAt(i);
@@ -16,4 +14,57 @@ function reversion(toReverseWord){
   return reversed;
 }
 
-  module.exports = sum,  reversion;
+const calculator =  {
+  add : function (a,b){
+  return a + b;
+},
+subtract : function (a,b){
+return a-b;
+},
+multiply : function(a,b){
+return 1*2;
+}
+
+}
+
+function caesarCipher(StringInput,shiftCases){
+  if (shiftCases == undefined)
+  shiftCases = 1;
+ let ciphred = '';
+const alphabet = buildAlphabet();
+for(let i in StringInput){
+  if(!(alphabet.includes(StringInput.charAt(i)))){
+    ciphred += StringInput.charAt(i);
+  }else{ 
+     let indexOfChar = (alphabet.indexOf(StringInput.charAt(i)));
+    
+  if(indexOfChar >= 25)
+  indexOfChar -=26;
+  if(StringInput.charAt(i) == (StringInput.charAt(i).toUpperCase())){
+    ciphred += ((alphabet[indexOfChar+shiftCases]).toUpperCase());
+    }else{
+
+ciphred += alphabet[indexOfChar+shiftCases];
+    }
+}
+}
+return ciphred;
+
+ }
+
+ function buildAlphabet(){
+  let alphaArray = []; alphaArray.length = 26;
+  for(let i = 0,  j = 65; i< alphaArray.length; i++,j++){
+    alphaArray[i] = String.fromCharCode(j).toLowerCase();
+  }
+  return alphaArray;
+ }
+
+ function wrappingCharacteres(toBeWrapped,shiftCases){
+  if((toBeWrapped+shiftCases)>25){
+    return (toBeWrapped -= 26);
+  }
+ }
+ 
+
+module.exports  = {reverseString , capitalize, calculator,caesarCipher};
--- practice.test.js
+++ practice.test.js
@@ -1,17 +1,51 @@
-const  sum = require('./practice.js');
-const reversion = require('./practice.js');
+let  practice = require('./practice.js');
 
-test('adds 1 + 2 to equal 3', () => {
-  expect(sum(1, 2)).toBe(3);
-});
-
-
-/*test('testWord turns into TestWord', () => {
-  let word = 'testWord';
-  expect(capitalize(word)).toBe('TestWord')
+test('Testing capitalize: testWord turns into TestWord', () => {
+  expect(practice.capitalize('testWord')).toBe('TestWord')
   
-});*/
-
-test('reversed turns into desrever', () => {
-  expect(reversion('reverse')).toEqual('desrever');
 });
+
+test('Testing reverseString: reversed turns into desrever', () => {
+  expect(practice.reverseString('reversed')).toEqual('desrever');
+});
+
+test('adding 1 and 2 = 3', () =>{
+  expect(practice.calculator.add(1,2)).toBe(3);
+});
+
+test('subtracting 1 and 2 = -1', () =>{
+  expect(practice.calculator.subtract(1,2)).toBe(-1);
+});
+
+test('multiply 1 and 2 = 2', () =>{
+  expect(practice.calculator.multiply(1,2)).toBe(2);
+});
+
+
+test('caesarCipher: "placeholder" has been shifted to "qmbdfipmefs" by 1',() =>{
+  expect(practice.caesarCipher("placeholder")).toBe("qmbdfipmefs");
+});
+
+test('caesarCipher: "renner" has been shifted to "tgppgt" by 2',() =>{
+  expect(practice.caesarCipher("renner",2)).toBe("tgppgt");
+});
+
+test('caesarCipher: wrapping "zebra" has been shifted to "afcsb" by 1',() =>{
+  expect(practice.caesarCipher("zebra",1)).toBe("afcsb");
+});
+test('caesarCipher: wrapping "zebra" has been shifted to "bgdtc" by 2',() =>{
+  expect(practice.caesarCipher("zebra",2)).toBe("bgdtc");
+});
+
+test('caesarCipher: puntuaction! wrapping "zebra..." has been shifted to "bgdtc..." by 2',() =>{
+  expect(practice.caesarCipher("zebra...",2)).toBe("bgdtc...");
+});
+
+test('caesarCipher: spaces! wrapping "zebra " has been shifted to "bgdtc " by 2',() =>{
+  expect(practice.caesarCipher("zebra ",2)).toBe("bgdtc ");
+});
+
+test('caesarCipher: Cases! wrapping "zEbRA... " has been shifted to "bGdTC... " by 2',() =>{
+  expect(practice.caesarCipher("zEbRA... ",2)).toBe("bGdTC... ");
+});
+
